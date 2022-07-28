1963cee9b58e8ce6e2b6c3a7b257255f93549a64
Josh Farrow <josh.farrow1992@outlook.com>
2022-07-28 10:49:31 +0100

    Animation Improved, append HTML To boxes when About is clicked

--- scripts.js
+++ scripts.js
@@ -2,12 +2,14 @@ const MainDiv = document.querySelector("#DivCollector");
 const DivTwo = document.querySelector("#DivCollectorTwo");
 const About = document.querySelector("#About");
 const Icon = document.querySelector("#IconCollector");
-const MainLogo = document.querySelector("#LogoCollector")
-const Title = document.querySelector("#H1Collector")
-const SubTitle = document.querySelector("#PCollector")
+const MainLogo = document.querySelector("#LogoCollector");
+const Title = document.querySelector("#H1Collector");
+const SubTitle = document.querySelector("#PCollector");
+const ProcessContent = document.querySelector("#Process");
+const PortfolioContent = document.querySelector("#Portfolio");
+const ContactContent = document.querySelector("#Contact");
 
 var ClickCount = 0;
-
 
 About.addEventListener("mouseover", function() {Mouse_Over()})
 About.addEventListener("mouseleave", function() {Mouse_Leave()})
@@ -21,14 +23,25 @@ function Mouse_Over() {
 }
 
 function Mouse_Leave() {
-    About.style.color = "";
-    About.style.opacity = "";
-    Icon.src = "/Portfolio/images/icons/arrow-down-circle-white.svg";
-    Icon.style.opacity = "";
+
+    isTrue = false;
+
+    if (ClickCount % 2 === 0) {
+        isTrue = true;
+    }
+
+    if (isTrue === true) {
+        About.style.color = "";
+        About.style.opacity = "";
+        Icon.src = "/Portfolio/images/icons/arrow-down-circle-white.svg";
+        Icon.style.opacity = "";}
+    
+    if (isTrue === false) {
+        return null
+    }
 }
 
 function Mouse_Down() {
-
 
     isTrue = false;
 
@@ -37,49 +50,100 @@ function Mouse_Down() {
     }
 
     if (isTrue == true) {
-    MainDiv.classList.add("ShrinkDiv");
-    DivTwo.classList.add("IncreaseDiv");
-    MainLogo.classList.add("LowerOpacity");
-    Title.classList.add("LowerOpacity");
-    SubTitle.classList.add("LowerOpacity");
+        MainDiv.classList.add("ShrinkDiv");
+        DivTwo.classList.add("IncreaseDiv");
+        MainLogo.classList.add("LowerOpacity");
+        Title.classList.add("LowerOpacity");
+        SubTitle.classList.add("LowerOpacity");
 
-    Title.textContent = "";
-    SubTitle.innerHTML = "";
+        About.style.color = "#000000";
+        About.style.opacity = "40%";
+        Icon.src = "/Portfolio/images/icons/arrow-down-circle-black.svg";
+        Icon.style.opacity = "40%";
 
-    ClickCount ++;
-    console.log(isTrue, ClickCount);
+        ProcessContent.innerHTML = PROCESS_HTML;
+        PortfolioContent.innerHTML = PORTFOLIO_HTML;
+        ContactContent.innerHTML = CONTACT_HTML;
+
+        ClickCount ++;
 
     }
 
     if (isTrue === false) {
 
-    MainDiv.classList.remove("ShrinkDiv");
-    DivTwo.classList.remove("IncreaseDiv");
-    MainLogo.classList.remove("LowerOpacity");
-    Title.classList.remove("LowerOpacity");
-    SubTitle.classList.remove("LowerOpacity");
+        MainDiv.classList.remove("ShrinkDiv");
+        DivTwo.classList.remove("IncreaseDiv");
+        MainLogo.classList.remove("LowerOpacity");
+        Title.classList.remove("LowerOpacity");
+        SubTitle.classList.remove("LowerOpacity");
 
-    Title.textContent = "Josh Farrow";
-    SubTitle.innerHTML = "&lt;Web Development & U.I. Design&gt; <span class='blink' id = 'BlinkCollector'>|</span>";
+        Title.textContent = "Josh Farrow";
+        SubTitle.innerHTML = "&lt;Web Development & U.I. Design&gt; <span class='blink' id = 'BlinkCollector'>|</span>";
 
 
-    MainDiv.classList.add("ShrinkDivRev");
-    DivTwo.classList.add("IncreaseDivRev");
-    MainLogo.classList.add("LowerOpacityRev");
-    Title.classList.add("LowerOpacityRev");
-    SubTitle.classList.add("LowerOpacityRev");
+        MainDiv.classList.add("ShrinkDivRev");
+        DivTwo.classList.add("IncreaseDivRev");
 
-    setTimeout(() => {
-        MainLogo.classList.remove("LowerOpacityRev");
-        Title.classList.remove("LowerOpacityRev");
-        SubTitle.classList.remove("LowerOpacityRev");
-        MainDiv.classList.remove("ShrinkDivRev"),
-        DivTwo.classList.remove("IncreaseDivRev")},
-        1000)
+        Title.classList.add("LowerOpacityRev");
+        SubTitle.classList.add("LowerOpacityRev");
+        MainLogo.classList.add("LowerOpacityRev");
 
-    ClickCount ++
-    console.log(isTrue, ClickCount)
+        About.style.color = "";
+        About.style.opacity = "";
+        Icon.src = "/Portfolio/images/icons/arrow-down-circle-white.svg";
+        Icon.style.opacity = "";
 
+        setTimeout(() => {
+            MainLogo.classList.remove("LowerOpacityRev");
+            Title.classList.remove("LowerOpacityRev");
+            SubTitle.classList.remove("LowerOpacityRev");
+            MainDiv.classList.remove("ShrinkDivRev");
+            DivTwo.classList.remove("IncreaseDivRev");
+            ProcessContent.innerHTML = "";
+            PortfolioContent.innerHTML = "";
+            ContactContent.innerHTML = "";},
+            1000)
+
+        ClickCount ++
     }
 
 }
+
+const PROCESS_HTML = `
+    <h2>The Process</h2>
+    <ol>
+        <li>Define</li>
+            <ul>
+                <li>Customer Requirements</li>
+            </ul>
+        <br>
+        <li>ProtoType</li>
+            <ul>
+                <li>Basic Development</li>
+            </ul>
+        <br>
+        <li>Review</li>
+            <ul>
+                <li>Customer Walkthrough and Feedback</li>
+            </ul>
+        <br>
+        <li>Execute</li>
+            <ul>
+                <li>Deployment Ready Development</li>
+            </ul>
+        <br>
+        <li>Deploy</li>
+            <ul>
+                <li>Customer Handover</li>
+            </ul>
+        <br>
+    </ol>
+`
+
+const PORTFOLIO_HTML = `
+    <h2>Portfolio Projects</h2>
+`
+
+const CONTACT_HTML = `
+    <h2>Contact Information</h2>
+`
